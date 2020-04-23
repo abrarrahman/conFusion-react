92893960fb31169e75f598d136f7dfdcfbfa9f8c
abrarrahman <abrarrahman97@gmail.com>
2020-04-23 15:12:33 +0600

    single page applications part-1

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,3 +1,34 @@
 import React from 'react';
-const Home = props => <h4>Home</h4>
+import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle} from 'reactstrap';
+
+const RenderCard = ({item}) => {
+  return(
+    <Card>
+      <CardImg src={item.image} alt={item.name}/>
+      <CardBody>
+        <CardTitle><h4>{item.name}</h4></CardTitle>
+        {item.designation ? <CardSubtitle><h5>{item.designation}</h5></CardSubtitle> : null}
+        <CardText>{item.description}</CardText>
+      </CardBody>
+    </Card>
+  )
+}
+
+const Home = props => {
+  return(
+    <div className="container">
+      <div className="row align-items-start">
+        <div className='col-12 col-md m-1'>
+          <RenderCard item={props.dish}/>
+        </div>
+        <div className='col-12 col-md m-1'>
+          <RenderCard item={props.promotion}/>
+        </div>
+        <div className='col-12 col-md m-1'>
+          <RenderCard item={props.leader}/>
+        </div>
+      </div>
+    </div>
+  )
+}
 export default Home;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,29 +1,38 @@
 import React, {Component} from 'react';
 import Home from '../Home/Home';
 import Menu from '../Menu/Menu';
+import Contact from '../Contact/Contact';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import { DISHES } from '../../shared/dishes';
+import { COMMENTS } from '../../shared/comments';
+import { LEADERS } from '../../shared/leaders';
+import { PROMOTIONS } from '../../shared/promotions';
 import {Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      selectedDishId: null,
+    this.state={
+      dishes: {DISHES},
+      promotions: {PROMOTIONS},
+      comments: {COMMENTS},
+      leaders: {LEADERS}
     }
   }
-  onDishSelect=dishId=>{
-    this.setState({selectedDishId: dishId});
-  }
   render(){
-    const HomePage = () => <Home/>
+    const HomePage = () => <Home 
+      dish={DISHES.filter(dish=>dish.featured)[0]}
+      promotion={PROMOTIONS.filter(promo=>promo.featured)[0]}
+      leader={LEADERS.filter(leader=>leader.featured)[0]}
+    />
     return (
       <div>
         <Header/>
         <Switch>
           <Route path="/home" component={HomePage}/>
           <Route exact path="/menu" component={()=> <Menu dishes={DISHES}/>}/>
+          <Route path="/contactus" component={Contact}/>
           <Redirect to="/home"/>
         </Switch>
         <Footer/>

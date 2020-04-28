eb05d596e2065d816bf04d1914b3f0ee4e814fae
abrarrahman <abrarrahman97@gmail.com>
2020-04-28 17:39:24 +0600

    react-redux-form revisited

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -9,6 +9,7 @@ import Footer from '../Footer/Footer';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {addComment, fetchDishes} from '../../redux/actionCreators';
+import {actions} from 'react-redux-form';
 
 class Main extends Component {
   componentDidMount(){
@@ -44,7 +45,7 @@ class Main extends Component {
           <Route exact path="/aboutus" component={()=><About leaders={this.props.leaders}/>}/>
           <Route exact path="/menu" component={()=> <Menu dishes={this.props.dishes}/>}/>
           <Route path="/menu/:dishId" component={DishWithId}/>
-          <Route exact path="/contactus" component={Contact}/>
+          <Route exact path="/contactus" component={()=><Contact resetFeedbackForm={this.props.resetFeedbackForm}/>}/>
           <Redirect to="/home"/>
         </Switch>
         <Footer/>
@@ -63,7 +64,8 @@ const mapStateToProps = state => {
 }
 const mapDispatchToProps = dispatch => ({
   addComment: (dishId,rating,author,comment) => dispatch(addComment(dishId,rating,author,comment)),
-  fetchDishes: () => dispatch(fetchDishes())
+  fetchDishes: () => dispatch(fetchDishes()),
+  resetFeedbackForm: () => dispatch(actions.reset('feedback'))
 })
 
 export default withRouter(connect(mapStateToProps,mapDispatchToProps)(Main));
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,8 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import {createForms} from 'react-redux-form';
+import {InitialFeedback} from './forms';
 import {Comments} from './comments';
 import {Leaders} from './leaders';
 import {Promotions} from './promotions';
@@ -11,7 +13,10 @@ export const ConfigureStore = () => {
     dishes: Dishes,
     comments: Comments,
     promotions: Promotions,
-    leaders: Leaders
+    leaders: Leaders,
+    ...createForms({
+      feedback: InitialFeedback
+    })
   })
   const store = createStore(reducers,applyMiddleware(thunk, logger));
   return store;
